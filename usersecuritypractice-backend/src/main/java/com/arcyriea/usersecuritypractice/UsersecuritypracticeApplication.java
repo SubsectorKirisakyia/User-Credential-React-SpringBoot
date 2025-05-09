5505c4db741baef8f5d936175123283604f3c8ab
SubsectorKirisakyia <108776309+SubsectorKirisakyia@users.noreply.github.com>
2025-05-09 18:17:12 +0700

    Setting up framework for new project practicing Spring Security with RestAPI

---
+++ usersecuritypractice-backend/src/main/java/com/arcyriea/usersecuritypractice/UsersecuritypracticeApplication.java
@@ -0,0 +1,13 @@
+package com.arcyriea.usersecuritypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersecuritypracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersecuritypracticeApplication.class, args);
+	}
+
+}
