f3ac5b9286d93c9c80bf9ee1b9cc97fe3616a5d2
SubsectorKirisakyia <108776309+SubsectorKirisakyia@users.noreply.github.com>
2025-05-12 14:29:13 +0700

    Reconfiguring Data Initialization mechanism

--- usersecuritypractice-backend/src/main/java/com/arcyriea/usersecuritypractice/database/DataInitializer.java
+++ usersecuritypractice-backend/src/main/java/com/arcyriea/usersecuritypractice/database/DataInitializer.java
@@ -1,11 +1,16 @@
 package com.arcyriea.usersecuritypractice.database;
 
+import java.io.File;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import com.arcyriea.usersecuritypractice.entity.Account;
+import com.arcyriea.usersecuritypractice.repository.AccountRepository;
+
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 
 
@@ -13,23 +18,44 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
 
-    private final JdbcTemplate jdbcTemplate;
+    private static final String INITIALIZED_FILE = "application_initialized.lock";
+    @Autowired
+    private AccountRepository userRepository;
+    // private final JdbcTemplate jdbcTemplate;
     @Autowired
     private PasswordEncoder encoder;
     
     @Override
     public void run(String... args) throws Exception {
-        try {
-            String password = "88888888";
-            jdbcTemplate.execute("INSERT INTO users (email, password)\r\n" + //
-                        "SELECT 'Artist001@art.com', '" + encoder.encode(password) + "'\r\n" + //
-                        "WHERE NOT EXISTS (SELECT 1\r\n" + //
-                        "    FROM users\r\n" + //
-                        "    WHERE email = 'Artist001@art.com');");
-        } catch (Exception e) {
-           System.err.println(e.getMessage());
-        }
         
     }
     
+    @PostConstruct
+    public void initialize(){
+        File initializedFile = new File(INITIALIZED_FILE);
+        if (!initializedFile.exists()){
+            try {
+            System.out.println("Initializing data... Adding Preconfigured Users");
+
+            Account user1 = new Account();
+            user1.setEmail("Artist001@art.com");
+            user1.setPassword(encoder.encode("88888888"));
+            System.out.println(user1.getEmail());
+
+
+            Account user2 = new Account();
+            user2.setEmail("Artist002@art.com");
+            user2.setPassword(encoder.encode("88888888"));
+            System.out.println(user2.getEmail());
+
+            userRepository.save(user1);
+            userRepository.save(user2);
+            initializedFile.createNewFile();
+        } catch (Exception e) {
+           System.err.println(e.getMessage());
+        }
+        } else {
+            System.out.println("Application's Database data already initialized");
+        }
+    }
 }
